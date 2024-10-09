1f2dc5c0d8db3699f9a1402c5f90ac28bab0a133
PhamHung2k4 <phamchunghung2@gmail.com>
2024-10-09 19:38:05 +0700

    first commit

---
+++ PhoneContext.js
@@ -0,0 +1,15 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const PhoneContext = createContext();
+
+export const PhoneProvider = ({ children }) => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  return (
+    <PhoneContext.Provider value={{ phoneNumber, setPhoneNumber }}>
+      {children}
+    </PhoneContext.Provider>
+  );
+};
+
+export const usePhone = () => useContext(PhoneContext);
---
+++ README.md
@@ -0,0 +1,2 @@
+![Kết quả bài tập](./61a420f7-b2ce-4a2c-a5f7-5bafb37b07d1.jpg)
+![Kết quả bài tập](./d78e0332-06ea-4b05-b1b6-79b5829feab6.jpg)
